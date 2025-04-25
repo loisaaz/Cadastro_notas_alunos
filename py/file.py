95ea187b47294584ef5e5eb5c3093cef2f137dc8
heloisa <125582181+loisaaz@users.noreply.github.com>
2025-04-25 12:13:23 -0300

    Update do código com instruções da lucia
    nao criamos os teoria [] e pratica
    nao adicionamos t1..p2 nelas
    e nao acrescentamos elas separadamente ao sistema.
    cada info deve ser inserida de forma separada para compreendermos melhor. nao há necessidade de info_alunos foi retirado após os ajustes certos e necessarios..
    o menu ja funciona corretamente. contudo, as informações nao aparecem tao bonitas, mas, esta funcional.

--- py/file.py
+++ py/file.py
@@ -16,6 +16,8 @@ for i in range(num_alunos):
     print(f"\nCADASTRO DO(A) ALUNO {i + 1}")
     nome = input("\nDIGITE O NOME DO ALUNO:")
 
+    TEORIA = []
+
     T1 = float(input("\nDIGITE A NOTA DA PROVA TEÓRICA (T1): "))
     while T1 < 0 or T1 > 10:
         print("\nNOTA INVÁLIDA!! DIGITE UM NÚMERO ENTRE 0 E 10.")
@@ -23,8 +25,13 @@ for i in range(num_alunos):
 
     T2 = float(input("\nDIGITE A NOTA DA PROVA TEÓRICA (T2): "))
     while T2 < 0 or T2 > 10:
-        print("\nNOTA INVÁLIDA!! DIGITE UM NÚMERO ENTRE 0 E 10.")
+        print("\nTNOTA INVÁLIDA!! DIGITE UM NÚMERO ENTRE 0 E 10.")
         T2 = float(input("\nDIGITE A NOTA DA PROVA TEÓRICA (T2): "))
+
+    TEORIA.append(T1)
+    TEORIA.append(T2)
+
+    PRATICA = []
 
     P1 = float(input("\nDIGITE A NOTA DE PROJETO (P1): "))
     while P1 < 0 or P1 > 10:
@@ -36,6 +43,9 @@ for i in range(num_alunos):
         print("\nNOTA INVÁLIDA!! DIGITE UM NÚMERO ENTRE 0 E 10.")
         P2 = float(input("DIGITE A NOTA DE PROJETO (P2): "))
 
+    PRATICA.append(P1)
+    PRATICA.append(P2)
+
     mt = (0.4 * T1) + (0.6 * T2)
     mp = (P1 + P2) / 2
     if mt > 5 and mp > 5:
@@ -44,14 +54,22 @@ for i in range(num_alunos):
         mf = mt
     else:
         mf = mp
-
+    MEDIATEORICA = [mt]
+    MEDIAPRATICA = [mp]
+    MEDIAFINAL = [mf]
     print('\n---------------------------------------------------------------------------------------------')
-    cadastro_alunos.append([
-        f"[{nome}], NOTAS TEÓRICAS: [{T1}], {T2}], NOTAS PRÁTICAS: [{P1}, {P2}] MÉDIA TEÓRICA E PRÁTICA: [{mt:.2f}, {mp:.2f}], MÉDIA FINAL: [{mf:.2f}]"])
+    cadastro_alunos.append(nome)
+    cadastro_alunos.append(TEORIA)
+    cadastro_alunos.append(PRATICA)
+    cadastro_alunos.append(MEDIATEORICA)
+    cadastro_alunos.append(MEDIAPRATICA)
+    cadastro_alunos.append(MEDIAFINAL)
+#    cadastro_alunos.append(aluno)
     print(f"\nCADASTRO DO(A) ALUNO(A) {nome} CONCLUÍDO COM SUCESSO!")
     print('\n---------------------------------------------------------------------------------------------')
 
 menu = 0
+
 while menu != 6:
     print("\n<< MENU >>")
     print('\n')
@@ -63,29 +81,32 @@ while menu != 6:
     print("6. Sair")
 
     menu = int(input("\nDigite sua escolha: "))
+
     if menu == 1:
         print("\nBOLETIM DOS ALUNOS:")
         for aluno in cadastro_alunos:
             print('\n')
             print(aluno)
-
-
-        # o menu 2 não está funcionando
-
-
-
+            # for i in range(0, len(cadastro_alunos), 6): \ if cadastro_alunos[i] == nome_aluno:
     elif menu == 2:
-        pesquisa_aluno = input('\nNome do(a) aluno(a): ')
+        nome_aluno = input('\nNome do(a) aluno(a): ')
         for aluno in cadastro_alunos:
-            if aluno[0] == pesquisa_aluno:
-                print(aluno)
-                break
+            cadastro_alunos == nome_aluno
+            print('\nINFORMAÇÕES DO ALUNO PESQUISADO')
+            print('\n')
+            print(aluno)
+            break
         else:
             print("Aluno(a) não encontrado no sistema.")
     elif menu == 3:
-
-        
+        maior_mf = 0
+        for aluno in cadastro_alunos:
+           # mf = aluno[-1] # ultimo elemento  
+            if mf > maior_mf:  
+                maior_mf = mf
+                aluno_maiormf = aluno[0] 
+                print(f"Aluno com maior média final: {aluno_maiormf}, Média Final: {maior_mf}")
 
 # O nome do aluno com maior Média Final (MF)
 # O nome do aluno com menor Média Final (MF)
-# Percentual dos alunos com Média Final (MF) superior a 5.0
+# Percentual dos alunos com Média Final (MF) superior a 
